25e5d024b97ff6c7b669f614f57876e53fe4227c
Maru375 <www980304@gmail.com>
2024-03-28 02:34:57 +0900

    [Feat]: UserRepository 에 조회 메소드 추가

--- src/main/java/live/smoothing/user/user/repository/UserRepository.java
+++ src/main/java/live/smoothing/user/user/repository/UserRepository.java
@@ -1,7 +1,16 @@
 package live.smoothing.user.user.repository;
 
+import live.smoothing.user.user.dto.response.UserDetailResponse;
+import live.smoothing.user.user.dto.response.UserSimpleResponse;
 import live.smoothing.user.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<UserSimpleResponse> findSimpleByUserId(String userId);
+
+    Optional<UserDetailResponse> findDetailByUserId(String userId);
+
 }
