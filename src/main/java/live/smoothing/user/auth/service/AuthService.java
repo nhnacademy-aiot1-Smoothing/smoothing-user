2bda2922d20523a93a4d97f99e612bb3529bec97
JaeHyeok29 <129154806+JaeHyeok29@users.noreply.github.com>
2024-03-28 15:34:29 +0900

    [Feat]: AuthController 작성

--- src/main/java/live/smoothing/user/auth/service/AuthService.java
+++ src/main/java/live/smoothing/user/auth/service/AuthService.java
@@ -5,10 +5,14 @@ import live.smoothing.user.auth.dto.AuthResponse;
 import live.smoothing.user.auth.dto.CreateAuthRequest;
 import live.smoothing.user.auth.dto.UpdateAuthRequest;
 
+import java.util.List;
+
 public interface AuthService {
     void createAuth(CreateAuthRequest request);
 
     AuthResponse getAuth(Long authId);
     void updateAuth(Long authId, UpdateAuthRequest request);
     void deleteAuth(Long authId);
+
+    List<AuthResponse> getAllAuths();
 }
