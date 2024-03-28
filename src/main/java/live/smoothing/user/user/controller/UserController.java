37a4517bc1a303b37c775e42bccc4073de27fc7d
Maru375 <www980304@gmail.com>
2024-03-28 19:10:11 +0900

    [Feat]: User Controller 구현
    - 유저 생성
    - 유저 조회 (간략/상세)
    - 유저 수정 (PW, User Info 수정을 분리)
    - 유저 삭제 (조회는 되지 않지만 DB 에는 남아있도록 구현)

---
+++ src/main/java/live/smoothing/user/user/controller/UserController.java
@@ -0,0 +1,55 @@
+package live.smoothing.user.user.controller;
+
+import live.smoothing.user.user.dto.request.UserCreateRequest;
+import live.smoothing.user.user.dto.request.UserInfoModifyRequest;
+import live.smoothing.user.user.dto.request.UserPWModifyRequest;
+import live.smoothing.user.user.dto.response.UserResponseTemplate;
+import live.smoothing.user.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> createUser(@RequestBody UserCreateRequest request) {
+
+        userService.createUser(request);
+        return ResponseEntity.ok("사용자 회원 가입 완료");
+    }
+
+    @GetMapping("/login")
+    public ResponseEntity<UserResponseTemplate> getUserSimpleInfo(@RequestHeader("X-USER-ID") String userId) {
+
+        return ResponseEntity.ok(userService.getUserSimpleInfo(userId));
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<UserResponseTemplate> getUserDetailInfo(@RequestHeader("X-USER-ID") String userId) {
+
+        return ResponseEntity.ok(userService.getUserDetailInfo(userId));
+    }
+
+    @PatchMapping("/profile")
+    public ResponseEntity<String> userInfoModify(@RequestHeader("X-USER-ID") String userId, @RequestBody UserInfoModifyRequest request) {
+        userService.modifyUserInfo(userId, request);
+        return ResponseEntity.ok("사용자 비밀번호 변경 완료");
+    }
+
+    @PatchMapping("/profile/password")
+    public ResponseEntity<String> userPasswordModify(@RequestHeader("X-USER-ID") String userId, @RequestBody UserPWModifyRequest request) {
+        userService.modifyUserPassword(userId, request);
+        return ResponseEntity.ok("사용자 비밀번호 변경 완료");
+    }
+
+    @DeleteMapping("/inactive")
+    public ResponseEntity<String> userInactive(@RequestHeader("X-USER-ID") String userId){
+        userService.deleteUser(userId);
+        return ResponseEntity.ok("사용자 비활성화 완료");
+    }
+}
