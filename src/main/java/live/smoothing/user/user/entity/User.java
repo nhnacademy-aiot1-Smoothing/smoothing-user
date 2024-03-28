ef8b2a80d4a3ed9b3fbd92ad93f1762bfc73e9fb
Maru375 <www980304@gmail.com>
2024-03-28 15:32:29 +0900

    [Feat]: User 엔티티 추가 구현
    - delete 상태 값을 통해 조회 제한 기능 추가
    - 유저 정보 수정 메소드 추가

--- src/main/java/live/smoothing/user/user/entity/User.java
+++ src/main/java/live/smoothing/user/user/entity/User.java
@@ -2,6 +2,8 @@ package live.smoothing.user.user.entity;
 
 import live.smoothing.user.userauth.entity.UserAuth;
 import lombok.*;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -9,7 +11,9 @@ import java.util.List;
 
 @Getter
 @Entity
+@DynamicUpdate
 @Table(name = "users")
+@Where(clause = "delete_state = false")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
@@ -26,14 +30,31 @@ public class User {
     @Column(name = "user_email")
     private String userEmail;
 
+    @Setter
+    @Column(name = "delete_state")
+    private boolean deleteState;
+
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     private List<UserAuth> userAuths = new ArrayList<>();
 
     @Builder
-    public User(String userId, String userPassword, String userName, String userEmail) {
+    public User(String userId, String userPassword, String userName, String userEmail, boolean deleteState) {
         this.userId = userId;
         this.userPassword = userPassword;
         this.userName = userName;
+        this.userEmail = userEmail;
+        this.deleteState = deleteState;
+    }
+
+    public void modifyUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public void modifyUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void modifyUserEmail(String userEmail) {
         this.userEmail = userEmail;
     }
 }
