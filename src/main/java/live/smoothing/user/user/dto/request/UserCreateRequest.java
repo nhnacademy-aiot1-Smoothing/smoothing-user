138bb1a6d249133f62fbe435b91175f97c26a5dd
Maru375 <www980304@gmail.com>
2024-03-29 11:25:03 +0900

    [Feat]: Password 인코딩 로직 추가

--- src/main/java/live/smoothing/user/user/dto/request/UserCreateRequest.java
+++ src/main/java/live/smoothing/user/user/dto/request/UserCreateRequest.java
@@ -1,7 +1,7 @@
 package live.smoothing.user.user.dto.request;
 
-import live.smoothing.user.userauth.dto.UserAuthRequest;
 import live.smoothing.user.user.entity.User;
+import live.smoothing.user.userauth.dto.UserAuthRequest;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,16 +12,17 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserCreateRequest {
 
+
     private String userId;
     private String userPassword;
     private String userName;
     private String userEmail;
     private List<UserAuthRequest> userAuths;
 
-    public User toEntity(){
+    public User toEntity(String hashingPw){
         return User.builder()
                 .userId(userId)
-                .userPassword(userPassword)
+                .userPassword(hashingPw)
                 .userName(userName)
                 .userEmail(userEmail)
                 .deleteState(false)
