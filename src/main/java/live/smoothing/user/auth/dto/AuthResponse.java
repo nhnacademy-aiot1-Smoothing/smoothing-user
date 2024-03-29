315c0fe498553e7fbf199638ef959bb2ac4a7b20
Maru375 <www980304@gmail.com>
2024-03-29 11:24:00 +0900

    [Refactor]: AuthResponse tdo 변경

--- src/main/java/live/smoothing/user/auth/dto/AuthResponse.java
+++ src/main/java/live/smoothing/user/auth/dto/AuthResponse.java
@@ -2,7 +2,6 @@ package live.smoothing.user.auth.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
@@ -10,8 +9,4 @@ public class AuthResponse {
 
     private Long authId;
     private String authInfo;
-
-    public AuthResponse(String authInfo) {
-        this.authInfo = authInfo;
-    }
 }
